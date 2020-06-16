69aecf385974ba3941948ee415abcee9f0be6ae4
Kiruse <neurokiruse@outlook.com>
2020-06-16 15:43:24 +0200

    Adopted VPECore error macro

--- src/Errors.jl
+++ src/Errors.jl
@@ -1,17 +1,5 @@
-export FontError
+export AbstractFlixUIError
 
 abstract type AbstractFlixUIError <: Exception end
-abstract type AbstractSimpleError <: AbstractFlixUIError end
 
-struct FontError <: AbstractSimpleError
-    message::AbstractString
-end
-FontError() = "generic font error"
-
-function Base.show(io::IO, err::AbstractSimpleError)
-    if length(err.message) == 0
-        write(io, "Font Error")
-    else
-        write(io, "Font Error: $(err.message)")
-    end
-end
+@makesimpleerror FontError AbstractFlixUIError "Font error"
